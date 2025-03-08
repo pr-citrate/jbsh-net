c586a8d1ab7d74486abd53f878473c03bb84b65a
pr-citrate <pr.citrate@gmail.com>
2025-03-08 18:58:08 +0900

    Chore: Add daisyUI and apply tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
+import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+import Navbar from "@/components/commons/Navbar";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400'],
+  subsets: ['latin'],
+});
+
 
 export const metadata: Metadata = {
   title: "Google OAuth example in Next.js",
 };
+
 
 export default function RootLayout({
   children,
@@ -11,8 +21,9 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${notoSansKR.className}`}>
     <body>
+    <Navbar/>
     {children}
     </body>
     </html>
